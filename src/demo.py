9ca8fc968a50f01becab483985884d2dbcfcba1f
Jules Talloen <jules.talloen@ml6.eu>
2021-12-29 15:19:24 +0100

    Update demo script

--- src/demo.py
+++ src/demo.py
@@ -95,7 +95,7 @@ def demo(configs):
 
     if configs.output_format == 'video':
         output_video_path = os.path.join(configs.save_demo_dir, 'result.mp4')
-        cmd_str = 'ffmpeg -f image2 -i {}/%05d.jpg -start_number {} -b 5000k -c:v mpeg4 {}'.format(
+        cmd_str = 'ffmpeg -f image2 -i {}/%06d.jpg -start_number {} -b 5000k -c:v mpeg4 {}'.format(
             os.path.join(configs.frame_dir), middle_idx + 1, output_video_path)
         print(cmd_str)
         os.system(cmd_str)
