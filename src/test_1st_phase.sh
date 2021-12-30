3e7f328625e5a8dc4b8cfc83b983d8307c4cfb10
Jules Talloen <jules.talloen@ml6.eu>
2021-12-30 10:13:23 +0100

    Add test_1st_phase.sh

---
+++ src/test_1st_phase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python3 test.py \
+  --working-dir '/home/jules/TTNet-Real-time-Analysis-System-for-Table-Tennis-Pytorch' \
+  --saved_fn 'ttnet_1st_phase-2' \
+  --gpu_idx 0 \
+  --batch_size 1 \
+  --pretrained_path ../checkpoints/ttnet_1st_phase-2/ttnet_1st_phase-2_epoch_30.pth \
+  --no_seg \
+  --no_local \
+  --no_event \
+  --smooth-labelling
