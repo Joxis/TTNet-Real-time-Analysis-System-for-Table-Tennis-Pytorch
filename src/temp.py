95e293bf4c38e1ed4568912c296a270954c49855
Jules Talloen <jules.talloen@ml6.eu>
2021-12-30 17:03:27 +0100

    Debug

--- src/temp.py
+++ src/temp.py
@@ -34,11 +34,11 @@ def infer(model, num=10):
         for i in range(num):
             images = torch.randint(size=(1, 27, 128, 320), low=0, high=255)
             normalized_images = normalize(images)
-            print(f"normalized images sum: {torch.sum(normalized_images)}")
+            print(f"normalized images: sum={torch.sum(normalized_images)}")
             pred_ball_global, global_features, out_block2, out_block3, out_block4, out_block5 = model(normalized_images)
             pred_ball_pos = get_prediction_ball_pos(pred_ball_global, w=320, thresh_ball_pos_prob=0.05)
             print(pred_ball_pos)
-            print(i, torch.sum(pred_ball_global), torch.mean(pred_ball_global))
+            print(f"pred ball global: sum={torch.sum(pred_ball_global)} mean={torch.mean(pred_ball_global)}")
             if last_output is not None:
                 print(f"{torch.sum(torch.eq(last_output, pred_ball_global))}/{pred_ball_global.numel()} equal elements")
             last_output = pred_ball_global
