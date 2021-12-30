628e8512fe2bd8f2367ee0b885d175de6666d80d
Jules Talloen <jules.talloen@ml6.eu>
2021-12-30 15:46:01 +0100

    Debug

--- src/temp.py
+++ src/temp.py
@@ -13,11 +13,12 @@ def normalize(x, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
 
 def main():
     """Run the ball detection model."""
-    for i in range(10):
-        ball_detection = BallDetection(9, 0.5)
-        normalized_images = normalize(torch.randn(1, 27, 128, 320))
-        pred_ball_global, global_features, out_block2, out_block3, out_block4, out_block5 = ball_detection(normalized_images)
-        print(i, pred_ball_global)
+    with torch.no_grad():
+        for i in range(10):
+            ball_detection = BallDetection(9, 0.5)
+            normalized_images = normalize(torch.randn(1, 27, 128, 320))
+            pred_ball_global, global_features, out_block2, out_block3, out_block4, out_block5 = ball_detection(normalized_images)
+            print(i, pred_ball_global)
 
 
 if __name__ == "__main__":
