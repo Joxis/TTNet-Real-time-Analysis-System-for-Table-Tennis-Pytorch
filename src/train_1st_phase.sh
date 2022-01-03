77ff198700998748b5f3bfeec4539a711dade221
Jules Talloen <jules.talloen@ml6.eu>
2022-01-03 14:11:56 +0100

    Debug

--- src/train_1st_phase.sh
+++ src/train_1st_phase.sh
@@ -8,8 +8,8 @@ python3 main.py \
   --num_workers 64 \
   --lr 0.001 \
   --lr_type 'step_lr' \
-  --lr_step_size 10 \
-  --lr_factor 0.1 \
+  --lr_step_size 5 \
+  --lr_factor 0.2 \
   --gpu_idx 0 \
   --global_weight 1. \
   --no_seg \
