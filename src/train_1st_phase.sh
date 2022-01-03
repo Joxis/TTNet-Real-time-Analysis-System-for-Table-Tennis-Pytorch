ba5c6903a1e7cf869fa8ab8f872a183712309988
Jules Talloen <jules.talloen@ml6.eu>
2022-01-03 17:31:48 +0100

    Debug

--- src/train_1st_phase.sh
+++ src/train_1st_phase.sh
@@ -2,11 +2,11 @@
 
 python3 main.py \
   --working-dir '/home/jules/TTNet-Real-time-Analysis-System-for-Table-Tennis-Pytorch' \
-  --saved_fn 'ttnet_1st_phase-8' \
+  --saved_fn 'ttnet_1st_phase-9' \
   --no-val \
   --batch_size 8 \
   --num_workers 64 \
-  --lr 0.001 \
+  --lr 0.0001 \
   --lr_type 'step_lr' \
   --lr_step_size 5 \
   --lr_factor 0.2 \
