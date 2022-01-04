e8ca2e00a7ffa508d7dbe42ab1f1ff27282c1a90
Jules Talloen <jules.talloen@ml6.eu>
2022-01-04 10:25:03 +0100

    Debug

--- src/test_3rd_phase.sh
+++ src/test_3rd_phase.sh
@@ -5,7 +5,7 @@ python3 test.py \
   --saved_fn 'ttnet_3rd_phase' \
   --gpu_idx 0 \
   --batch_size 1 \
-  --pretrained_path ../checkpoints/ttnet_3rd_phase/ttnet_3rd_phase_epoch_30.pth \
+  --pretrained_path ../checkpoints/ttnet_phase3/ttnet_phase3_epoch_30.pth \
   --seg_thresh 0.5 \
   --event_thresh 0.5 \
   --smooth-labelling
