193adbcc9291c27702ae48683ac25e7bd006527b
Jules Talloen <jules.talloen@ml6.eu>
2022-01-04 10:57:32 +0100

    Debug

--- src/train.sh
+++ src/train.sh
@@ -36,7 +36,7 @@ python3 main.py \
   --seg_weight 0. \
   --event_weight 2. \
   --local_weight 1. \
-  --pretrained_path ../checkpoints/ttnet_1st_phase/ttnet_1st_phase_epoch_30.pth \
+  --pretrained_path ../checkpoints/ttnet_1st_phase/ttnet_phase1_epoch_30.pth \
   --overwrite_global_2_local \
   --freeze_seg \
   --freeze_global \
@@ -59,5 +59,5 @@ python3 main.py \
   --seg_weight 1. \
   --event_weight 1. \
   --local_weight 1. \
-  --pretrained_path ../checkpoints/ttnet_2nd_phase/ttnet_2nd_phase_epoch_30.pth \
+  --pretrained_path ../checkpoints/ttnet_2nd_phase/ttnet_phase2_epoch_30.pth \
   --smooth-labelling
