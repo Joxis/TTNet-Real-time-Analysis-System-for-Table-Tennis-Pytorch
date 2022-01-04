3e97ebd23a355f322a1eafc6fafa0cdbc4a300b6
Jules Talloen <jules.talloen@ml6.eu>
2022-01-04 14:24:43 +0100

    Debug

--- src/demo.sh
+++ src/demo.sh
@@ -5,7 +5,7 @@ python3 demo.py \
   --saved_fn 'demo' \
   --arch 'ttnet' \
   --gpu_idx 0 \
-  --pretrained_path ../checkpoints/ttnet_3rd_phase/ttnet_3rd_phase_epoch_30.pth \
+  --pretrained_path ../checkpoints/ttnet_phase2/ttnet_phase2_epoch_30.pth \
   --seg_thresh 0.5 \
   --event_thresh 0.5 \
   --thresh_ball_pos_mask 0.05 \
