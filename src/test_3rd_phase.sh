688d7170993cf336c704db234e9732de5a5ee382
Jules Talloen <jules.talloen@ml6.eu>
2022-01-05 09:32:22 +0100

    Debug

--- src/test_3rd_phase.sh
+++ src/test_3rd_phase.sh
@@ -2,10 +2,10 @@
 
 python3 test.py \
   --working-dir '/home/jules/TTNet-Real-time-Analysis-System-for-Table-Tennis-Pytorch' \
-  --saved_fn 'ttnet_3rd_phase' \
+  --saved_fn 'ttnet_multi' \
   --gpu_idx 0 \
   --batch_size 1 \
-  --pretrained_path ../checkpoints/ttnet_phase3/ttnet_phase3_epoch_30.pth \
+  --pretrained_path ../checkpoints/ttnet_multi/ttnet_multi_epoch_30.pth \
   --seg_thresh 0.5 \
   --event_thresh 0.5 \
   --smooth-labelling
